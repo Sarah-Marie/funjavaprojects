95352e8b6f5d03691c9732604edcb03fcc8016a9
Sarah Marie <sarwmsn@gmail.com>
2020-08-05 13:34:38 -0400

    init commit

---
+++ src/com/sarahmarie/Main.java
@@ -0,0 +1,8 @@
+package com.sarahmarie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sarah: dependable, grit, life-long learner, polygot");
+    }
+}
